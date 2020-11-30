c2ca2eb84eb5ef2cea973826bf8e25b48381aa08
carolinearvidsson <72596642+carolinearvidsson@users.noreply.github.com>
2020-11-30 17:00:57 +0100

    ändring

--- features.py
+++ features.py
@@ -4,7 +4,7 @@ from wordspace import WS
 from domainspecificity import DomainSpecificity
 from embeddings import Embeddings
 from frequency import Frequency
-from ngram_nltk_v2 import NgramN
+from char_ngram import NgramN
 from pos import PosTagger
 from sen_len import SenLen
 
@@ -34,11 +34,11 @@ class FeatureMatrix:
         self.matrix.append(features)
 
 if __name__ == "__main__":
-    train_data = ['homemade_train.tsv']
+    train_data = ['data/homemade_train.tsv']
     ws = WS(train_data)
     freqdata = '/Users/carolinearvidsson/googlebooks-eng-all-1gram-20090715-*.txt'
-    fclsses = (NgramN(), DomainSpecificity(ws), Frequency(freqdata), Embeddings(ws))
+    fclsses = (NgramN(), SenLen(PosTagger(ws)), DomainSpecificity(ws), Frequency(freqdata), Embeddings(ws))
     m = FeatureMatrix(fclsses)
-    for wordobj in ws.single_word:
-        m.populate_matrix(wordobj)
-    pickle.dump(m, open('matrix_train', 'wb'))
+    #for wordobj in ws.single_word:
+    #    m.populate_matrix(wordobj)
+    #pickle.dump(m, open('matrix_train', 'wb'))
