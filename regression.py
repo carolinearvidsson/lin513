ba5c4b0c3d3e5312c3bfc6a133d1af91070ab659
Christoffer Forbes Schieche <72505590+cschieche@users.noreply.github.com>
2020-12-04 10:54:03 +0100

    Update regression.py

--- regression.py
+++ regression.py
@@ -11,15 +11,15 @@ class MultiLinear():
         train_raw = pickle.load(open(train_feature_matrix, 'rb'))
         train_matrix = train_raw.matrix
         train_compl = train_raw.complexities
-        # train = [[],train_raw[1]]
-        # for i, entry in enumerate(train_raw[0]):
-        #     emb = entry[5]
-        #     try:
-        #         emba = emb.tolist()
-        #         entry = entry[:5] + emba[50:101]
-        #         train[0].append(entry)
-        #     except:
-        #         train[1].pop(i)
+        train = [[],train_compl]
+        for i, entry in enumerate(train_matrix):
+            emb = entry[5]
+            try:
+                emba = emb.tolist()
+                entry = entry[:5] + emba[50:101]
+                train[0].append(entry)
+            except:
+                train[1].pop(i)
             
             
             #print(entry)
@@ -30,16 +30,17 @@ class MultiLinear():
         test_raw = pickle.load(open(test_feature_matrix, 'rb'))
         test_matrix = test_raw.matrix
         test_compl = test_raw.complexities
-        # for i, entry in enumerate(test_raw[0]):
-        #     emb = entry[5]
-        #     try:
-        #         emba = emb.tolist()
-        #         entry = entry[:5] + emba[50:101]
-        #         test[0].append(entry)
-        #     except: 
-        #         test[1].pop(i)
+        test = [[], test_compl]
+        for i, entry in enumerate(test_matrix):
+            emb = entry[5]
+            try:
+                emba = emb.tolist()
+                entry = entry[:5] + emba[50:101]
+                test[0].append(entry)
+            except: 
+                test[1].pop(i)
         #print(train,test)
-        print(self.__predict(self.__train_linear_model(train_matrix, train_compl), test_matrix, test_compl))
+        print(self.__predict(self.__train_linear_model(train[0], train[1]), test[0], test[1]))
 
     def __train_linear_model(self, train_matrix, train_compl):
 
