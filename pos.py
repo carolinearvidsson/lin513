12b43776cb9e2cb50c217cd85b551564e7aa691d
Christoffer Forbes Schieche <72505590+cschieche@users.noreply.github.com>
2020-12-07 16:51:56 +0100

    Update pos.py

--- pos.py
+++ pos.py
@@ -36,8 +36,9 @@ class PosTagger:
                 self.tagged_sentences[entry.id] = [tagged_sent, token_sent_index, token_pos_id]
             except:
                 high_pos_tag = max(self.tag_counter, key=self.tag_counter.get)
+                high_pos_tag_id = self.pos_id[high_pos_tag]
                 high_token_sent_index = max(self.token_index_counter, key=self.token_index_counter.get) 
-                self.tagged_sentences[entry.id] = [nltk.pos_tag(sentence), high_token_sent_index, high_pos_tag] #här blir det fel!!!!   
+                self.tagged_sentences[entry.id] = [nltk.pos_tag(sentence), high_token_sent_index, high_pos_tag_id] #här blir det fel!!!!   
 
     def __create_tag_ids(self, pos_tag):
         self.pos_id.setdefault(pos_tag, len(self.pos_id))
@@ -45,5 +46,33 @@ class PosTagger:
     def get_pos(self, wordobject):
         pos_id = self.tagged_sentences[wordobject.id][2]
         return [pos_id]
+    
+    def get_sen_len(self, wordobject):
+        '''Get length of sentence up to (not including) token. 
+        Return two values: all_sen_len counts all words preceeding token,
+        lex_sen_len counts only lexical/content words.
+        
+        Arguments:
+            wordobject: 
+        '''
+
+        upenn_content_tags = ['CD','JJ', 'JJR', 'JJS', 'NN', 'NNS', 
+                                   'NNP','NNPS', 'RB', 'RBR', 'UH', 'VB', 
+                                   'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+        sen_id = wordobject.id
+        sentence = self.tagged_sentences[wordobject.id]
+        all_sen_len = sentence[1]
+
+        try:
+            lex_sen_len = 0
+            token_ind = sentence[1]
+            preceeding = sentence[0][:token_ind]
+            for word, tag in preceeding:
+                if tag in upenn_content_tags:
+                    lex_sen_len += 1
+        except:
+            lex_sen_len = sentence[1]
+        return [all_sen_len, lex_sen_len]
 
 
