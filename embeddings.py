99c372225ff4612f249e1b56cf3b7814b418fb72
carolinearvidsson <72596642+carolinearvidsson@users.noreply.github.com>
2020-12-16 16:05:08 +0100

    documentation for embeddings DONE?!?!?!

--- embeddings.py
+++ embeddings.py
@@ -20,12 +20,38 @@ from bert_embedding import BertEmbedding
 
 
 class Embeddings:
+  '''Generates BERT embeddings and uses them to perform word sense induction
+  on observations of word types.
 
+  All public methods (that do not start with leading underscore) are used to 
+  return one or more feature(s) of a given word object. These features
+  are used to predict lexical complexity of a word in context.
+  
+  Parameters:
+  
+    ws (worspace object)
+      A wordspace class object.
+      
+    embfile (str)
+      The path of a pickle format file holding the embeddings. If file 
+      does not exist, it will be created and the process of generating
+      embeddings will be initialized.
+      For a detailed rescription of the contents of this file,
+      see the documentation for local method: __setup.
+      
+  Attributes:
+
+    self.all_targets (set)
+      A set of all target word types in the wordspace object.
+
+    self.wnl (wordnet lemmatizer object)
+      Will be used to lemmatize words.
+      '''
 
   def __init__(self, ws, embfile):
-    self.wnl = WordNetLemmatizer()
     self.all_targets = ws.target_types
     self.embfile = embfile
+    self.wnl = WordNetLemmatizer()
     self.__check_existing_file()
 
   def __check_existing_file(self):
@@ -125,7 +151,7 @@ class Embeddings:
       for wtype in self.all_targets:
         if wtype in tokens:
           # Get index of all target words in the sentence.
-          type_indices = [i for i, token in enumerate(tokens) if token == wtype]
+          type_indices = [i for i, tokn in enumerate(tokens) if tokn == wtype]
           wtype = self.wnl.lemmatize(wtype)
           for index in type_indices:
             self.lemma_embs.setdefault(wtype, []).append(sen_emb[index])
@@ -166,7 +192,7 @@ class Embeddings:
     '''Computes the mean of each column in all the target tokens' 
     embeddings in order to get an "average embedding".
     These dimension means will be returned as features of target tokens
-    whose embeddings could not be retrieved.
+    whose embeddings could not be retrieved during setup.
     '''
     all_target_embeddings = [self.tID_emb[token] for token in \
                 self.tID_emb if self.tID_emb[token] is not None]
@@ -184,11 +210,13 @@ class Embeddings:
         Holds lemmatized word types (str) as keys and their estimated
         number of clusters as values. The number of clusters will be 
         greater than 1 only if the 'optimal clustering' 
-        generated a silhouette score above 0.25.
+        generated a silhouette score above 0.25. 
+        Number of clusters is defined as the total number of clusters, 
+        excluding the number of cluster outliers.
       
       self.cluster_outliers (set)
         Holds all embeddings of tokens that are
-        the only members in a cluster.
+        the only members of a cluster.
 
       pdist_matrx (ndarray)
         A condensed distance matrix. This is needed to create clusters.
@@ -235,7 +263,7 @@ class Embeddings:
         This is a value between -1 and 1. Values near 1 indicate that
         the flat clusters are well-defined. Values near 0 suggest overlapping
         clusters. Values near -1 indicate that samples within a cluster
-        have been erroneously assigned to the wrong cluster. 
+        have been assigned to the wrong cluster. 
         Using this program, no silhouette score
         under 0 have been observed. This might be because the flat clusters
         are derived from hierarchical clusters created through
@@ -264,6 +292,29 @@ class Embeddings:
     return max_score, best_clusters
 
   def __get_outliers(self, clusters):
+    '''Finds if any observation in flat clustering
+    is an only member of its cluster. These observations are
+    regarded as outliers.
+
+    Parameters:
+
+      clusters (ndarray)
+        An array of length N where N is the number of observations.
+        C[o] is the cluster number (np.uint64) to which observation o belongs.
+
+      outlier_indices (list)
+        Each element is the index (int) of an outlier. 
+        Importantly, the indices of each observation correspond to 
+        the indices in the list values in self.lemma_embs.
+
+    Returns:
+    
+      n_outliers (int)
+        Number of outliers in the flat clusters.
+        
+      outlier_indices (list)
+        The index (int) of each cluster outlier
+    '''
     clusters = clusters.tolist()
     outlier_indices = [clusters.index(obs) for obs in set(clusters) \
                                             if clusters.count(obs) == 1]
@@ -271,12 +322,20 @@ class Embeddings:
     return n_outliers, outlier_indices
 
   def target_emb(self, wobj):
+    '''Returns the embedding (list) of a given word object.
+    If the embedding could not be retrieved during setup, an
+    average embedding is returned
+    '''
     embedding = self.tID_emb[wobj.id]
     if embedding is None:
       embedding = self.average_embedding
     return embedding.tolist()
 
   def n_clusters(self, wobj):
+    '''Returns the number of clusters of a given word object's
+    lemma (int). If the number of clusters cannot be retrieved
+    due to the absence of the word object's embedding, an average
+    number of clusters is returned'''
     try:
       lemma = self.wnl.lemmatize(wobj.token.lower())
       return [int(self.n_clusters[lemma])]
@@ -286,6 +345,7 @@ class Embeddings:
       return [float(average_n_clusters)]
 
   def is_cluster_outlier(self, wobj):
+    '''Returns 1 if a wordobject is a cluster outlier and 0 if it is not.'''
     is_outlier = 0
     embedding = self.tID_emb[wobj.id]
     if embedding is not None and tuple(embedding) in self.cluster_outliers: 
